2039eb9b35716c9d4db1e4ee9d30c37dfcfddb4f
Aymerick772 <Aymerick772@gmail.com>
2023-02-09 16:39:48 +0400

    reset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 import React, {Component} from 'react'
-import Mycars from './components/Mycars';
 
 class App extends Component {
 
@@ -33,11 +32,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <Mycars title={this.state.titre}/>
-        <button onClick={this.changeTitle}> changer le nom en dure</button>
-        <button onClick={() => this.changeViaParam("title via le param")}> changer le nom dynamique</button>
-        <button onClick={this.changeViaBind.bind(this, "nouveau titre via bind")}> changer le nom via bind</button>
-        <input type="text" onChange={this.changeViaInput} value={this.state.titre}/>
+  
       </div>
     );  
   }
