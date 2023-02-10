0aba79fb69d920b47f67d805e09fbccca13d462c
Aymerick772 <Aymerick772@gmail.com>
2023-02-10 16:16:02 +0400

    cours 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 //learn 2
 import React, {Component} from 'react'
+import Mom from './components/Mom'
 
 class App extends Component {
 
   render(){
     return (
       <div className="App">
-  
+        <h1>Maman</h1>
+        <Mom/>
+        <hr/>
+        <h1>Enfant</h1>
       </div>
     );  
   }
---
+++ src/components/Mom.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Mom extends Component {
+    state = {
+        messageMom : null,
+        messageToto: null,
+    }
+    render() {
+        return(
+            <div className="">
+
+            </div>
+        )
+    }
+}
+
+export default Mom;
