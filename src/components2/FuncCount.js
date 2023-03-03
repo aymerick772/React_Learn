c5494d93a1f1b0d92197ebe9ea393bc00dfab8c5
Aymerick772 <Aymerick772@gmail.com>
2023-03-03 16:24:44 +0100

    question performance du useEffect pour la fonction et componentDidUpdate pour la class

--- src/components2/FuncCount.js
+++ src/components2/FuncCount.js
@@ -2,17 +2,25 @@ import React, {useState, useEffect} from "react";
 
 function FuncCount () {
 const [count, setCount ] = useState(0);
+const [name, setName ] = useState('');
+
+
 useEffect(() =>{
+    console.log('maj titr');
     setTimeout(()=>{
         document.title = `count : ${count}`
     }, 2000)
-})
+    //le [count] sert ne pas executer la fonction useEffect si le count reste le même
+}, [count])
+
+
 
 
     return (
         <div className="">
             <h1>Count {count}</h1>
             <button onClick={()=> setCount(count + 1)}> Cliquer</button>
+            <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
         </div>
     )
 }
