aff9684dbe9368dffd8e8c90aea9470e00c4d1c2
Aymerick772 <Aymerick772@gmail.com>
2023-03-04 19:08:55 +0100

    useReducer avec une chaine caractère

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ClassCount from './components2/ClassCount';
 import FuncCount from './components2/FuncCount';
 import Container from './components3/Container';
 import Profile from './components4/Profile';
-
+import Count from './components5/Count';
 import {UserContext, ColorContext} from './components4/MyContext';
 
 class App extends Component {
@@ -28,12 +28,12 @@ state = {
      {/* <Container /> component 3 */}
       {/*component 4 */}
 
-     <UserContext.Provider value={this.state.user}>
+     {/* <UserContext.Provider value={this.state.user}>
         <ColorContext.Provider value='red'>
           <Profile />
        </ColorContext.Provider>
-      </UserContext.Provider>
-
+      </UserContext.Provider> Component 4 */}
+        <Count/>
       </div>
     );  
   }
