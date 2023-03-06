4926707b4aa4cc0a4a2e5669ff893b0c496389e2
Aymerick772 <Aymerick772@gmail.com>
2023-03-06 16:38:19 +0100

    optimisation application avec useMemo()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import React, {Component} from 'react'
 
+
 // import App1UseCallBack from './components/App1UseCallBack'
 import App2UseMemo from './components2/App2UseMemo'
 
@@ -10,7 +11,7 @@ class App extends Component {
   render() {
     return (
 
-      <div className="App">
+      <div className="">
           {/* <App1UseCallBack/> */}
           <App2UseMemo/>
       </div>
--- src/components2/App2UseMemo.js
+++ src/components2/App2UseMemo.js
@@ -1,15 +1,65 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect, useMemo} from 'react'
+import Profile from './Profile'
 
-class App2UseMemo extends Component {
+import axios from 'axios'
 
-  render() {
+//Pour facilité la création d'app pour voir chaque Hooks de react, nous avons plusieur composant avec chaque app
+function App2UseMemo(){
+
+  const [count, setCount] = useState(1);
+  const [profile, setProfile] = useState({});
+  const [dark, setDark] = useState(false);
+
+  useEffect(()=>{ 
+    axios
+    .get(`https://jsonplaceholder.typicode.com/users/${count}`)
+    .then((response)=>{
+      // console.log(response.data);
+      setProfile(response.data);
+    })
+    .catch(err=>{
+      console.log('error')
+    })
+  }, [count]);
+
+
+  //optimiser avec useMemo, si le count change alors 
+  const isOverTen = useMemo( ()=>{
+    console.log('je suis dans isOverTen')
+    
+    return count > 10
+  }, [count])
+
+  const goDark = ()=>{
+    setDark(!dark)
+
+
+    if(!dark){
+      document.body.classList.add("bg-secondary");
+    }else{
+      document.body.classList.remove("bg-secondary");  
+    }
+  }
+
+  const classBtnTheme = dark ? 'btn-light' : 'btn-dark';
+  const textBtnTheme = dark ? 'rendre claire' : 'render sombre';
+
+
     return (
 
       <div className="container">
+        <h1 className="text-center">useMemmo()</h1>
 
-      </div>
+        {isOverTen && <div className="alert alert-danger" role="alert">STOP !!</div>}
+
+        <button className="btn btn-info m-3" onClick={()=> setCount(count + 1)}>Incrémenter {count}</button>
+
+        <button className={`btn ${classBtnTheme} m-5`} onClick={goDark}>{textBtnTheme}</button>
+        
+        <Profile profile={profile} count={count}/>
+      </div>  
     );  
   }
-}
+
 
 export default App2UseMemo;
