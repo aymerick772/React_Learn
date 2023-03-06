879c627dd7fd8f49dce8bc3343ed2d6fe2db338f
Aymerick772 <Aymerick772@gmail.com>
2023-03-06 13:41:08 +0100

    Init App2UseMemo

---
+++ src/components2/App2UseMemo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class App2UseMemo extends Component {
+
+  render() {
+    return (
+
+      <div className="container">
+
+      </div>
+    );  
+  }
+}
+
+export default App2UseMemo;
